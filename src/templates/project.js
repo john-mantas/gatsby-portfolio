633f8b5918612817ecf9e0314d24d43ad64c1313
John Mantas <mantasjohn67@gmail.com>
2020-05-18 00:49:28 +0300

    feat: Update project template with new content

--- src/templates/project.js
+++ src/templates/project.js
@@ -4,18 +4,28 @@ import { graphql } from 'gatsby'
 import MetaData from '../components/meta/MetaData'
 import Layout from '../components/common/Layout'
 
+import HeroProject from '../components/blocks/HeroProject'
+import BodyProject from '../components/blocks/BodyProject'
+
+import '../styles/pages/project.scss'
+
 export default ({ data, location }) => {
-  const doc = data.prismic.allProjects.edges.slice(0, 1).pop().node;
+  const doc = data.prismic.allProjects.edges.slice(0, 1).pop();
   if (!doc) return null;
 
   return (
     <>
       <MetaData
-        doc={doc}
+        doc={doc.node}
         location={location}
       />
       <Layout>
-        <pre>{JSON.stringify(doc, null, 4)}</pre>
+        <article>
+          <HeroProject item={doc.node} />
+          <div className="body-project container boundary--around">
+            <BodyProject body={doc.node.body} />
+          </div>
+        </article>
       </Layout>
     </>
   )
